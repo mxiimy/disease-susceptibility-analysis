1b19ca3ce6b64e04945cc7ea7dcbb46cc9934bf6
mxiimy <momelody33@gmail.com>
2024-09-29 12:25:03 -0400

    Added background, reformatted to look nicer, added country to hover

--- Front_End/Heat_Map.py
+++ Front_End/Heat_Map.py
@@ -2,13 +2,32 @@ import plotly.graph_objects as go
 import pandas as pd
 import plotly.io as pio
 
-# Load your dataset
 data = pd.read_csv('AQI and Lat Long of Countries.csv')
+sample_data = pd.read_csv('data.csv')
 
-# Initialize a blank figure
 fig = go.Figure()
 
-# Add NO2 AQI trace (visible by default)
+# Add Actual Data (visible by default)
+fig.add_trace(go.Scattermapbox(
+    lat=sample_data['lat'],
+    lon=sample_data['lng'],
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=sample_data['Sample_Susceptibility'],
+        color=sample_data['Sample_Susceptibility'],
+        colorscale='Viridis',
+        sizemode='area',
+        sizeref=2.*max(sample_data['Sample_Susceptibility'])/(15.**2),
+        colorbar=dict(title="Sample Susceptibility"),
+    ),
+    visible=True,
+    hovertemplate='<b>Sample Susceptibility</b> <br>'+
+                  '<b>Coordinates:</b> (%{lon:.2f}, %{lat:.2f})<br>' +
+                  '<b>Value:</b> %{marker.color:.2f}<br>' +
+                  '<extra></extra>',
+))
+
+# Add NO2 AQI trace (hidden initially)
 fig.add_trace(go.Scattermapbox(
     lat=data['lat'],
     lon=data['lng'],
@@ -21,11 +40,16 @@ fig.add_trace(go.Scattermapbox(
         sizeref=2.*max(data['NO2 AQI Value'])/(15.**2),
         colorbar=dict(title="NO2 AQI"),
     ),
-    name='NO2 AQI',
-    visible=True  # Visible by default
+    visible=False,
+    hovertemplate='<b>NO2 AQI</b> <br>'+
+                  '<b>Coordinates:</b> (%{lon:.2f}, %{lat:.2f})<br>' +
+                  '<b>Value:</b> %{marker.color:.2f}<br>' +
+                  '<b>Country:</b> %{customdata}<br>'+
+                  '<extra></extra>',
+    customdata=data['Country']
 ))
 
-# Add CO AQI trace (hidden initially)
+# Repeat for CO AQI trace
 fig.add_trace(go.Scattermapbox(
     lat=data['lat'],
     lon=data['lng'],
@@ -38,11 +62,16 @@ fig.add_trace(go.Scattermapbox(
         sizeref=2.*max(data['CO AQI Value'])/(15.**2),
         colorbar=dict(title="CO AQI"),
     ),
-    name='CO AQI',
-    visible=False  # Hidden by default
+    visible=False,
+    hovertemplate='<b>CO AQI</b> <br>' +
+                  '<b>Coordinates:</b> (%{lon:.2f}, %{lat:.2f})<br>' +
+                  '<b>Value:</b> %{marker.color:.2f}<br>' +
+                  '<b>Country:</b> %{customdata}<br>' +
+                  '<extra></extra>',
+    customdata=data['Country']
 ))
 
-# Add Ozone AQI trace (hidden initially)
+# Ozone AQI trace (hidden initially)
 fig.add_trace(go.Scattermapbox(
     lat=data['lat'],
     lon=data['lng'],
@@ -55,11 +84,15 @@ fig.add_trace(go.Scattermapbox(
         sizeref=2.*max(data['Ozone AQI Value'])/(15.**2),
         colorbar=dict(title="Ozone AQI"),
     ),
-    name='Ozone AQI',
-    visible=False  # Hidden by default
+    visible=False,
+    hovertemplate='<b>Ozone AQI</b> <br>' +
+                  '<b>Coordinates:</b> (%{lon:.2f}, %{lat:.2f})<br>' +
+                  '<b>Value:</b> %{marker.color:.2f}<br>' +
+                  '<b>Country:</b> %{customdata}<br>'+
+                  '<extra></extra>',
+    customdata=data['Country']
 ))
 
-# Set up the layout, including the map style
 fig.update_layout(
     mapbox=dict(
         style="carto-positron",
@@ -68,23 +101,29 @@ fig.update_layout(
     ),
     margin={"r": 0, "t": 0, "l": 0, "b": 0},
     height=600,
-
-    # Add dropdown menu buttons to switch between datasets
+    paper_bgcolor='rgba(0, 0, 0, 0)',
+    plot_bgcolor='rgba(0, 0, 0, 0)',
+    font=dict(color='white'),
     updatemenus=[
         {
             "buttons": [
                 {
-                    "args": [{"visible": [True, False, False]}],  # Show NO2 AQI only
+                    "args": [{"visible": [True, False, False, False]}],
+                    "label": "Our data",
+                    "method": "update"
+                },
+                {
+                    "args": [{"visible": [False, True, False, False]}],
                     "label": "NO2 AQI",
                     "method": "update"
                 },
                 {
-                    "args": [{"visible": [False, True, False]}],  # Show CO AQI only
+                    "args": [{"visible": [False, False, True, False]}],
                     "label": "CO AQI",
                     "method": "update"
                 },
                 {
-                    "args": [{"visible": [False, False, True]}],  # Show Ozone AQI only
+                    "args": [{"visible": [False, False, False, True]}],
                     "label": "Ozone AQI",
                     "method": "update"
                 }
@@ -95,15 +134,5 @@ fig.update_layout(
     ]
 )
 
-fig.update_traces(
-    hovertemplate='<b>Coordinates:</b> (%{lon:.2f}, %{lat:.2f})<br>' +  # Longitude, Latitude
-                  '<b>AQI Value:</b> %{marker.color:.2f}<br>' +  # The value you're displaying (AQI Value in this case)
-                  '<extra></extra>',  # Removes the "trace" label from the hover box
-)
-
-
-# Save the map as an HTML file
 pio.write_html(fig, '../map_with_buttons.html')
-
-# Optional: Display the map in the browser
 fig.show()
